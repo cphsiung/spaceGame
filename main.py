fb0610d77a6e3c9f13c016693e4a67dcc22d6193
cphsiung <chunpeihs@gmail.com>
2021-08-09 16:15:26 -0700

    Add sprite settings

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
 
 FPS = 60
-WHITE = (255,255,255)
 WIDTH = 500
 HEIGHT = 600
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+
 # Initiate pygame and set window sizes
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-running = True
+pygame.display.set_caption("Space Game")
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = 200
+        self.rect.y = 200
+
+    def update(self):
+        self.rect.x += 2
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+running = True
 while running:
     
     clock.tick(FPS) # Max update FPS times in one sec
@@ -21,9 +42,11 @@ while running:
             running = False
 
     # Update game
+    all_sprites.update()
 
     # Display game
     screen.fill(WHITE)
+    all_sprites.draw(screen)
     pygame.display.update()
-    
+
 pygame.quit()
